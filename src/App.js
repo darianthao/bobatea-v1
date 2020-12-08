2ef873dc920d0d70f1a9eaf5c32a93f3a7c476e2
unknown <justdarian01@gmail.com>
2020-12-08 07:11:53 -0600

    Added NavBar Basics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import OrderNowFunction from './orderNowFunction';
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App(){
 
   return(
     <div className="app">
+      <Navbar />
       <h1 className="welcomeLogo">Welcome</h1>
       <h2 className="order-now"> <OrderNowFunction  /></h2>
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { MenuItems } from "./MenuItems"
+import './Navbar.css'
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className="NavbarItems"> </nav>
+            <nav className="NavbarItems">
+            <h1 className="navbar-logo">React</h1>
+                <div className="menu-icon">
+
+                </div>
+                <ul> 
+                    {MenuItems.map((item, index) => {
+                        return(
+                    <li>
+                        <a className={item.cName} href={item.url}>
+                            {item.title}
+                        
+                        </a>
+                    </li>
+                        )
+                })}
+                    
+                </ul>
+            </nav>
         )
     }
 }
+
+export default Navbar
