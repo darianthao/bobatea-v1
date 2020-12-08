371906c2c913c862cc97a881fd58501d962ea8f6
unknown <justdarian01@gmail.com>
2020-12-08 08:44:31 -0600

    Updated NavBar.css and added button functions

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems"
+import { Button } from "../Button"
 import './Navbar.css'
 
 class Navbar extends Component {
+
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked })
+    }
+
     render() {
         return (
             <nav className="NavbarItems">
-            <h1 className="navbar-logo">React</h1>
-                <div className="menu-icon">
-
+            <h1 className="navbar-logo">Boba Tea<i className="fab fa-react"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul> 
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}> 
                     {MenuItems.map((item, index) => {
                         return(
                     <li>
@@ -23,6 +31,7 @@ class Navbar extends Component {
                 })}
                     
                 </ul>
+                <Button>Sign Up</Button>
             </nav>
         )
     }
